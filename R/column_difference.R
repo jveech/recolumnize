726d7498490d1a27784836f952d6615be06720a0
John Veech <jveech@stat.tamu.edu>
2019-11-30 22:36:12 -0600

    refactor column_difference() to handle more situations

--- R/column_difference.R
+++ R/column_difference.R
@@ -1,11 +1,12 @@
-column_difference <- function(encode_cols, all_cols) {
+column_difference <- function(encode_cols, all_cols, num_cols) {
   # function returns boolean vector of columns not in encode_cols
-  num_cols <- length(all_cols)
   if (!is.null(encode_cols)) {
-    if (typeof(encode_cols) == "integer") {
+    if (typeof(encode_cols) == "integer" | typeof(encode_cols) == "double") {
       keep_cols <- !1:num_cols %in% encode_cols
+      encode_cols <- !keep_cols
     } else if (typeof(encode_cols) == "character") {
       keep_cols <- !all_cols %in% encode_cols
+      encode_cols <- !keep_cols
     } else {
       stop('Invalid encode columns provided, need to provide either names or indices of columns')
     }

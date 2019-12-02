bcca46cfc19410c3992d290d1bc09849c2647ac8
John Veech <jveech@stat.tamu.edu>
2019-12-02 12:09:03 -0600

    Make new function to handle reordering rows in recategorize() which is more graceful when dataset has duplicate or missing categories.

--- R/recategorize.R
+++ R/recategorize.R
@@ -1,7 +1,7 @@
 #' Title
 #'
-#' @param df df dataframe that contains the data we want to recategorize
-#' @param encode_cols encode_cols which columns should be altered
+#' @param df dataframe that contains the data we want to recategorize
+#' @param enc which columns should be altered
 #' @param category_dictionary named list with a mapping from values to the categories they belong to
 #' @param ignore_unknown_values T if unknown values should be ignored, F if they should be one hot encoded
 #' @param handle_duplicate_categories "first" takes first value in a certain category, "last" takes last value
@@ -15,23 +15,28 @@
 #' "c","c","v","c","c","c","c","c","v",
 #' "c","c","c","c","c","v","c","c","c","c","c")
 #' names(dict) <- letters
-#' recategorize(my_mat, category_dictionary = dict)
-recategorize <- function(df, encode_cols = NULL, category_dictionary, ignore_unknown_values = T, handle_duplicate_categories ="first") {
+#' recategorize(my_mat,c(1,2), category_dictionary = dict)
+recategorize <- function(df, enc = NULL, category_dictionary,
+                         ignore_unknown_values = T, handle_duplicate_categories ="first") {
   #TODO compatability checks for category_dictionary
-  out <- column_difference(encode_cols, names(df))
+  out <- column_difference(enc, names(df), ncol(df))
   encode_cols  <- out$encode_cols
   keep_cols <- out$keep_cols
+  to_encode <- as.matrix(df[,encode_cols,drop=F])
 
   groups <- unique(dict)
-  num_cats <- length(unique(dict))
-  categ_indices <- t(apply(
-    cbind(matrix(match(sapply(df,category_from_dictionary,category_dictionary,USE.NAMES=F),
-                       groups),dim(df)),
-                                 matrix(1:num_cats,nrow=nrow(df),ncol=num_cats, byrow=T)),1,unique))
-  ordering <- t(apply(categ_indices,1,order))
-  for (i in 1:nrow(df)) {
-    df[i,] <- df[i, ordering[i,]]
-  }
-  colnames(df) <- groups
+  to_encode <- t(apply(to_encode, 1, reorder_row_by_group, category_dictionary, groups))
+  colnames(to_encode) <- groups
+  df <- cbind(df[,keep_cols,drop=F], to_encode)
   return(df)
+}
+
+reorder_row_by_group <-  function(vec, dict, groups) {
+  group_indices <- match(as.character(dict[vec]),groups)
+  group_indices[duplicated(group_indices)] <- NA
+
+  remaining_indices <- (1:length(groups))[!1:length(groups) %in% group_indices]
+  na_indices <- is.na(group_indices)
+  replaced <- order(replace(group_indices,na_indices, remaining_indices))
+  (replace(vec,na_indices,NA))[replaced]
 }

a2ae409d251fdaa69fddabab3b4e5e9c40ea4e73
John Veech <jveech@stat.tamu.edu>
2019-12-07 11:32:50 -0600

    create helper function best_categories_by_row()

--- R/best_categories_approximate.R
+++ R/best_categories_approximate.R
@@ -16,4 +16,18 @@ best_categories_approximate <- function(df, category_probabilities, encode_cols 
   return(t(apply(df,1, function(x) best_categories_approximate_by_row(dict[unlist(x),]))))
 }
 
+best_categories_approximate_by_row <- function(probs) {
+  ncat <- ncol(probs)
+  ordered_row <- rep(NA, ncat)
+  names(ordered_row) <- names(probs)
+  likelihood <- 1
+  for (i in 1:(ncat-1)) {
+    out <- get_best_index_from_row(probs)
+    ordered_row[out$col] <- rownames(probs)[out$max_index]
+    probs <- probs[-out$max_index,-out$col_index,drop = F]
+  }
+  ordered_row[names(probs)[1]] <- rownames(probs)[1]
+  return(ordered_row)
+}
+
 

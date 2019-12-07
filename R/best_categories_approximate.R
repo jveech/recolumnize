bfe82813a1ba75e488db3211da14958aa105cb58
John Veech <jveech@stat.tamu.edu>
2019-12-07 15:04:04 -0600

    Fix some issues with possible duplications in rows in best_category_approximate()

--- R/best_categories_approximate.R
+++ R/best_categories_approximate.R
@@ -26,35 +26,50 @@
 best_categories_approximate <- function(df, category_probabilities, encode_cols = NULL) {
   num_rows <- nrow(df)
   num_cols <- ncol(df)
+  out <- column_difference(encode_cols, colnames(df), num_cols)
   encode_cols  <- out$encode_cols
   keep_cols <- out$keep_cols
   to_be_encoded <- df[,encode_cols, drop = F]
   if (sum(!(unique(as.vector(as.matrix(to_be_encoded))) %in% rownames(category_probabilities) > 0))) {
     stop('Not all values found in category_probabilities, did you make sure that encode_cols are correct?')
   }
-  encoded <- (t(apply(to_be_encoded,1, function(x) best_categories_approximate_by_row(dict[unlist(x),]))))
+  encoded <- (t(apply(to_be_encoded,1,
+                      function(x) best_categories_approximate_by_row(x,
+                                                                     category_probabilities[unlist(unique(x)),]))))
   colnames(encoded) <- colnames(category_probabilities)
   return(cbind(df[,keep_cols],encoded))
 }
 
-best_categories_approximate_by_row <- function(probs) {
+best_categories_approximate_by_row <- function(row, probs) {
   ncat <- ncol(probs)
+  if(is.null(ncat)) {
+    return(row)
+  }
   ordered_row <- rep(NA, ncat)
-  names(ordered_row) <- names(probs)
+  names(ordered_row) <- colnames(probs)
   likelihood <- 1
   for (i in 1:(ncat-1)) {
+    if (nrow(probs) == 1){
+      break
+    }
     out <- get_best_index_from_row(probs)
     ordered_row[out$col] <- rownames(probs)[out$max_index]
-    probs <- probs[-out$max_index,-out$col_index,drop = F]
+    if (sum(row == rownames(probs)[out$max_index]) > 1) {
+      row <- row[-out$max_index]
+      probs <- probs[ ,-out$col_index,drop = F]
+    }
+    else {
+      probs <- probs[-out$max_index,-out$col_index,drop = F]
+    }
   }
-  ordered_row[names(probs)[1]] <- rownames(probs)[1]
+  ordered_row[is.na(ordered_row)] <- rownames(probs)[1]
   return(ordered_row)
 }
 
 
 get_best_index_from_row <- function(dict) {
   first_col <- which.max(apply(dict,2,find_biggest_ratio))
-  return(list(col = names(dict)[first_col], col_index = first_col, max_index = which.max(dict[,first_col])))
+  return(list(col = colnames(dict)[first_col], col_index = first_col, max_index = which.max(dict[,first_col])))
 }
 
 find_biggest_ratio <- function(vec) {

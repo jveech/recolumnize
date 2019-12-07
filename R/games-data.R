03cba6cf42241c4eda57e3d604f070b64a23a1e8
John Veech <jveech@stat.tamu.edu>
2019-12-06 22:38:02 -0600

    Add R file documentation for games_data

---
+++ R/games-data.R
@@ -0,0 +1,17 @@
+#' Recolumnize data example
+#'
+#' Data from 2500 League of Legends games. Collected via official API.
+#'
+#' @docType data
+#'
+#' @usage data(games_small)
+#'
+#' @format A dataframe
+#'
+#' @keywords datasets
+#'
+#' @source \href{"https://developer.riotgames.com/apis"}{Riot Games API}
+#'
+#' @examples
+#' data(games_small)
+"games_small"

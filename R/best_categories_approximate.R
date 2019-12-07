69fdb433278124011973aa5e3627c0ae7c4b601b
John Veech <jveech@stat.tamu.edu>
2019-12-07 15:03:29 -0600

    Improve documentation and add example to best_categories_approximate()

--- R/best_categories_approximate.R
+++ R/best_categories_approximate.R
@@ -1,19 +1,31 @@
-#' Title
+#' best_categories_approximate
 #'
-#' @param df
-#' @param category_probabilities
-#' @param encode_cols
+#' @param df data frame (or matrix) to be encoded
+#' @param category_probabilities matrix or dataframe with rownames containing keys to be looked up, ith column containing probabilities of being in category i
+#' @param encode_cols which columns should be encoded (others are left alone)
 #'
-#' @return
+#' @return dataframe with encode_cols replaced by data encoded into categories from caegory_probabilities
 #' @export
 #'
 #' @examples
+#' dict2 <- rep("consonant",26)
+#' names(dict2) <- letters
+#' dict2[c("a","e","i","o","u")] <- "vowel"
+
+#' probs <- matrix(0,nrow = 26, ncol = 2)
+#' colnames(probs) <- c("vowel","consonant")
+#' rownames(probs) <- letters
+#' probs[,1] <- abs((dict2 == "vowel") -.001)
+#' probs[25,1] <- 0.25
+#' probs[23,1] <- 0.05
+#' probs[,2] <- 1-probs[,1]
+#' mat <- matrix(c("a","w","x","y","c","w","r","r"),nrow = 4, ncol = 2, byrow=T)
+#' mat
+#' best_categories_approximate(mat,probs)
+
 best_categories_approximate <- function(df, category_probabilities, encode_cols = NULL) {
-  # TODO compatability checks
-  df <- as.data.frame(df) # convert tibbles, matrices and the like
   num_rows <- nrow(df)
   num_cols <- ncol(df)
-  out <- column_difference(encode_cols, names(df), num_cols)
   encode_cols  <- out$encode_cols
   keep_cols <- out$keep_cols
   to_be_encoded <- df[,encode_cols, drop = F]

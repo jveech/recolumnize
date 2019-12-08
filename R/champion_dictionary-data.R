9263c036a638fd9b42f09a7c5a97c603cac55d80
John Veech <jveech@stat.tamu.edu>
2019-12-07 19:49:39 -0600

    Add champion_dictionary.R

---
+++ R/champion_dictionary-data.R
@@ -0,0 +1,17 @@
+#' Recolumnize dictionary example
+#'
+#' Dictionary corresponding to games_small dataset
+#'
+#' @docType data
+#'
+#' @usage data(champ_dictionary)
+#'
+#' @format A dataframe
+#'
+#' @keywords datasets
+#'
+#' @source \href{"https://developer.riotgames.com/apis"}{Riot Games API}
+#'
+#' @examples
+#' data(champ_dictionary)
+"champ_dictionary"
